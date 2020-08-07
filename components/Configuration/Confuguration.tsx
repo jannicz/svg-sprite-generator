35f517787147062d4e6511dea7c5cc2dc2421c93
Suwart, Jan <j.suwart@gmail.com>
2020-08-07 16:27:25 +0200

    Add Dropzone node module and Configuration component, add test assets (material icons)

---
+++ components/Configuration/Confuguration.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Configuration.module.scss';
+
+const Configuration = () => {
+
+  return (
+    <header className={styles.header}>
+      <h2>
+        Configure output
+      </h2>
+
+    </header>
+  );
+}
+
+export default Configuration;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,30 @@
 import { NextPage, NextPageContext } from 'next';
+import { DropzoneArea } from 'material-ui-dropzone';
 import React from 'react';
 import Layout from '../components/Layout/Layout';
+import Configuration from '../components/Configuration/Confuguration';
 
-const Index: NextPage = (props: any) => {
+const Index: NextPage = () => {
+  const handleChange = (e) => {
+    console.log('Dropzone change', e);
+  };
 
   return (
     <Layout>
-      <h2>Simply create a SVG symbols file by dropping your icons here</h2>
-
-      <textarea rows={20} cols={80}></textarea>
+      <div>
+        <h2>Simply create a SVG symbols file by transforming your icons here</h2>
+        <div>
+          <DropzoneArea
+            onChange={handleChange.bind(this)}
+            acceptedFiles={['image/svg+xml']}
+            maxFileSize={5000000}
+            filesLimit={10000}
+          />
+        </div>
+        <div>
+          <Configuration />
+        </div>
+      </div>
     </Layout>
   )
 }
